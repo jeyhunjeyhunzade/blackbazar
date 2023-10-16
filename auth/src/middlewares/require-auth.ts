d0c82c98fcd62dc2f30dbc773bb48a1a65269897
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-16 23:45:47 +0300

    created require-auth middleware

---
+++ auth/src/middlewares/require-auth.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+
+import { NotAuthorizedError } from "../errors/not-authorized";
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.currentUser) {
+    throw new NotAuthorizedError();
+  }
+
+  next();
+};
