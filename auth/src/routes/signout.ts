a01115caa0b45bfbc782c9cfc8d0685740ac4173
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-16 23:15:45 +0300

    created signout method

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
-router.post("/api/users/signout", (req, res) => {
-  res.send("Signout api...");
+
+router.post("/api/users/signout", (req: Request, res: Response) => {
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
