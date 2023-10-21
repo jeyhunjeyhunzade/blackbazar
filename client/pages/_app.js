f6356adf8fc42f066d04af4f00395e79a4a14018
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-21 20:06:07 +0300

    created higher app getinitialprops context & integrated signout api

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import buildClient from "../api/buildClient";
+import Header from "../components/Header";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  let pageProps = {};
+
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
---
+++ client/pages/auth/signout.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import Router from "next/router";
+
+import { useRequest } from "../../hooks/useRequest";
+
+const SignOut = () => {
+  const { doRequest } = useRequest({
+    url: "/api/users/signout",
+    method: "post",
+    body: {},
+    onSuccess: () => Router.push("/"),
+  });
+
+  useEffect(() => {
+    doRequest();
+  }, []);
+
+  return <div>Signing user out...</div>;
+};
+
+export default SignOut;
