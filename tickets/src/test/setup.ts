6eb4587649018ac157cc88ea9f2214f60de041b4
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-24 00:51:23 +0300

    created tickets service , added create ticket method and its tests

---
+++ tickets/src/test/setup.ts
@@ -0,0 +1,49 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import request from "supertest";
+import jwt from "jsonwebtoken";
+
+import { app } from "../app";
+
+declare global {
+  var getCookie: () => string[];
+}
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = "lnBEduLkT4";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  const connections = await mongoose.connection.db.collections();
+
+  for (let collection of connections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.getCookie = () => {
+  const payload = {
+    id: "12enkjwkdqk",
+    email: "test1@test.com",
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`session=${base64}`];
+};
