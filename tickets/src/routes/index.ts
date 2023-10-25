667ef2cc2da7447ccf59351712baac805a99a232
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-25 20:32:54 +0300

    added get tickets/ticket methods and its tests

---
+++ tickets/src/routes/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from "express";
+import { NotFoundError } from "@blackbazar/common";
+
+import { Ticket } from "../models/ticket";
+
+const router = express.Router();
+
+router.get("/api/tickets", async (req: Request, res: Response) => {
+  const tickets = await Ticket.find({});
+
+  res.send(tickets);
+});
+
+export { router as indexTicketRouter };
