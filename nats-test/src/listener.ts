c2bcde6b489b339650d4c2ec82dbe3c3f0a2907b
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-28 01:37:06 +0300

    tested ackmode & durable name

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,31 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 console.clear();
 
-const stan = nats.connect("blackbazar", "123", {
+const stan = nats.connect("blackbazar", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  stan.on("close", () => {
+    console.log("NATS connection closed");
+    process.exit();
+  });
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName("orders-service");
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "queue-group-name",
+    options
+  );
 
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
@@ -17,5 +33,10 @@ stan.on("connect", () => {
     if (typeof data === "string") {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
+
+    msg.ack();
   });
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
