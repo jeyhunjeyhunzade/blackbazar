2fbfd4ee5a75fc365503d16b2727aa025e72ed86
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-31 00:23:54 +0300

    added nats client publisher tests for tickets service

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import mongoose from "mongoose";
 
 import { app } from "../../app";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("returns a 404 if the provided id does not exist", async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -78,6 +79,7 @@ it("updates the ticket provided valid inputs", async () => {
   const updatedTitle = "Updated title";
   const updatedPrice = 100;
   const cookie = global.getCookie();
+
   const response = await request(app)
     .post("/api/tickets")
     .set("Cookie", cookie)
@@ -101,4 +103,29 @@ it("updates the ticket provided valid inputs", async () => {
 
   expect(ticketResponse.body.title).toEqual(updatedTitle);
   expect(ticketResponse.body.price).toEqual(updatedPrice);
+});
+
+it("publishes an event", async () => {
+  const updatedTitle = "Updated title";
+  const updatedPrice = 100;
+  const cookie = global.getCookie();
+
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title: "New ticket",
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set("Cookie", cookie)
+    .send({
+      title: updatedTitle,
+      price: updatedPrice,
+    })
+    .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
