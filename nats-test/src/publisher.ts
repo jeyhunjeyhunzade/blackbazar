ba13610d7b7886778f64fdd188aea518f0fdbe47
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-10-30 21:17:29 +0300

    initialized nats publisher for tickets server

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,21 +1,24 @@
 import nats from "node-nats-streaming";
 
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
+
 console.clear();
 
 const stan = nats.connect("blackbazar", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   console.log("Publisher connected to NATS");
 
-  const data = JSON.stringify({
-    id: "123",
-    title: "concert",
-    price: 20,
-  });
-
-  stan.publish("ticket:created", data, () => {
-    console.log("Event published");
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: "123",
+      title: "Concert",
+      price: 20,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 const run = async () => {
   if (!process.env.JWT_KEY) {
@@ -12,6 +13,17 @@ const run = async () => {
   }
 
   try {
+    await natsWrapper.connect(
+      "blackbazar",
+      "qdsad1wdasdaamn",
+      "http://nats-srv:4222"
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
