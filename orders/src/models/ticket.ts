2e53d54790cea8eb37ba2b01709ff33cb89aa0e0
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-11-11 00:36:41 +0300

    created order & related ticket model

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+interface TicketAttributes {
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttributes): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttributes) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
