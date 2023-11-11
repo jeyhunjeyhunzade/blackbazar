1c3f33e5fcc5dbca6de5745518af32b7619f54d4
Jeyhun Jeyhunzade <mrjeyhun.work@gmail.com>
2023-11-11 20:40:50 +0300

    initialized ticket created & ticket updated listeners on orders service

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 import { app } from "./app";
 import { natsWrapper } from "./nats-wrapper";
+import { TicketCreatedListener } from "./events/listeners/ticket-created-listener";
+import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 
 const run = async () => {
   if (!process.env.JWT_KEY) {
@@ -36,6 +38,10 @@ const run = async () => {
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to MongoDB");
   } catch (err) {
